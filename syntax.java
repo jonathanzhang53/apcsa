2982101d6b88ee46e023f03910ce2c50bf61d250
Jonathan Zhang <41968151+jonathanzhang53@users.noreply.github.com>
2019-05-06 22:41:55 -0500

    Update syntax.java

--- syntax.java
+++ syntax.java
@@ -8,11 +8,34 @@ public class Syntax {
     // float, short, long, char are also primitives 
     
     /* CASTING */
+    double cast = (int) (15 / 4); // cast = 3 because it evaluates inside the parentheses and then casts it
+    double castBetter = (int) (15.0 / 4); // castBetter = 3 because it truncates the decimal when casting to int
+    // For casting objects, you want to make sure that the right side "is a" left side (see below)
+      // (Manager) (employeeObject) does not work because not all employees are managers [employee "is a" manager is false]
+    // For initializing objects, use the same strategy
+    Employee bob = new Manager(); // this works [manager "is a" employee is true]
+    // Watch out for inheritance and interface relationships, as you cannot initialize something across these classes
     
     /* STRINGS */
+    String owo = "AP-CSA "
+    int len = owo.length(); // len = 7 (spaces and symbols count)
+    int index = owo.indexOf("A"); // index = 0, since strings start at index 0 and the FIRST occurence is at 0
+    int substring = owo.substring(3,5); // substring = "CS", since it displays the first index parameter up to the second paramter index BUT does not display the last index
+    boolean comparison = owo.equals("AP-CSA1"); // comparison = false due to nonequality (DO NOT USE == for strings since they are objects)
+    // Remember to treat a String as an object, not a primitive
     
-    /* ARRAYS AND ARRAYLISTS */
+    /* ARRAYS */
+    // Direct Initialization
+    int[] grades = {98, 90, 56};
+    // Initialization
+    int[] gradebook = int[25]; // int array with 25 terms (index 0-24)
+    Grade[] individual = Grade[25]; // same thing but with an object
+    // Accessing
+    int aGrade = gradebook[3]; // sets aGrade to the int at the 3rd index of gradebook (4th term)
+    int lenArr = gradebook.length; // lenArr = 25, no parentheses following length
     
+    /* ARRAYLISTS */
+      
     /* LOOPS */
     
     /* IF, ELSE IF, ELSE */
