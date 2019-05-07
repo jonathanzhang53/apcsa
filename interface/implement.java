37b6f4ced25a37d0c41756e849755692693696ba
Jonathan Zhang <41968151+jonathanzhang53@users.noreply.github.com>
2019-05-06 22:05:07 -0500

    Update implement.java

--- interface/implement.java
+++ interface/implement.java
@@ -1,1 +1,21 @@
-
+// Basically just a regular class, but methods must match exactly with interface
+public class AnImplementation implements AnInterface {
+  private boolean gender;
+  private int age;
+  
+  public AnImplementation(int agein, boolean maleOrFemale) {
+    age = agein;
+    gender = maleOrFemale;
+  }
+  
+  // Changes gender
+  public void genderSet(boolean in) {
+    gender = in;
+  }
+  
+  // Ages by 1 year and returns new age
+  public int ageUp() {
+    age++;
+    return age;
+  }
+}
