a1b5e5653889803e2b91f66195e916124d318479
Jonathan Zhang <41968151+jonathanzhang53@users.noreply.github.com>
2019-05-07 20:24:47 -0500

    Update superclass.java

--- inheritance/superclass.java
+++ inheritance/superclass.java
@@ -1,5 +1,21 @@
+// Standard class with instance variables, constructor, accessor, mutator
 public class Super {
-  public Super(int x, int y, String z) {
+  private int num1;
+  private int num2;
+  private String tag;
+  private String combined;
   
+  public Super(int x, int y, String z) {
+    num1 = x;
+    num2 = y;
+    tag = z;
+  }
+  
+  public int getSum() {
+    return num1 + num2;
+  }
+  
+  public void concatenate() {
+    combined = tag + num1 + num2;
   }
 }
